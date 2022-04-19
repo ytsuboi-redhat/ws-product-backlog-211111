4afe604a47d1c5c06fd5502a7e125f50d4f672ad
Yosuke Tsuboi <ytsuboi@redhat.com>
2022-04-19 14:32:57 +0900

    UT 対応

--- backend/src/test/java/com/redhat/jp/labs/sample/backend/service/ProductBacklogServiceTest.java
+++ backend/src/test/java/com/redhat/jp/labs/sample/backend/service/ProductBacklogServiceTest.java
@@ -20,40 +20,46 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 public class ProductBacklogServiceTest {
 
-    @Mock
-    private ProductBacklogItemRepository productBacklogItemRepository;
+  @Mock
+  private ProductBacklogItemRepository productBacklogItemRepository;
 
-    @InjectMocks
-    private ProductBacklogService productBacklogService;
+  @InjectMocks
+  private ProductBacklogService productBacklogService;
 
-    @Test
-    public void getBacklog_whenLimitOffsetSpecified_thenApplyAsIs() {
-        ProductBacklogItem expected1 = new ProductBacklogItem(1L, "name1", "desc1", "1", "memo1");
-        ProductBacklogItem expected2 = new ProductBacklogItem(2L, "name2", "desc2", "2", "memo2");
-        List<ProductBacklogItem> list = Lists.newArrayList(expected1, expected2);
+  @Test
+  public void getBacklog_whenLimitOffsetSpecified_thenApplyAsIs() {
+    ProductBacklogItem expected1 = new ProductBacklogItem(1L, "name1", "desc1", "1", "memo1");
+    ProductBacklogItem expected2 = new ProductBacklogItem(2L, "name2", "desc2", "2", "memo2");
+    List<ProductBacklogItem> list = Lists.newArrayList(expected1, expected2);
 
-        doReturn(list).when(productBacklogItemRepository).find(100, 200);
+    doReturn(list)
+        .when(productBacklogItemRepository).find(100, 200);
 
-        assertThat(productBacklogService.getBacklog(100, 200)).containsExactly(expected1, expected2);
-    }
+    assertThat(productBacklogService.getBacklog(100, 200))
+        .containsExactly(expected1, expected2);
+  }
 
-    @Test
-    public void getBacklog_whenLimitIsNull_thenApplyDefault() {
-        ProductBacklogItem expected = new ProductBacklogItem(3L, "name", "desc", "3", "memo");
+  @Test
+  public void getBacklog_whenLimitIsNull_thenApplyDefault() {
+    ProductBacklogItem expected = new ProductBacklogItem(3L, "name", "desc", "3", "memo");
 
-        int limitDefault = 20;
-        doReturn(Collections.singletonList(expected)).when(productBacklogItemRepository).find(limitDefault, 5);
+    int limitDefault = 20;
+    doReturn(Collections.singletonList(expected))
+        .when(productBacklogItemRepository).find(limitDefault, 5);
 
-        assertThat(productBacklogService.getBacklog(null, 5)).contains(expected, atIndex(0));
-    }
+    assertThat(productBacklogService.getBacklog(null, 5))
+        .contains(expected, atIndex(0));
+  }
 
-    @Test
-    public void getBacklog_whenOffsetIsNull_thenApplyZero() {
-        ProductBacklogItem expected = new ProductBacklogItem(4L, "name", "desc", "4", "memo");
+  @Test
+  public void getBacklog_whenOffsetIsNull_thenApplyZero() {
+    ProductBacklogItem expected = new ProductBacklogItem(4L, "name", "desc", "4", "memo");
 
-        doReturn(Collections.singletonList(expected)).when(productBacklogItemRepository).find(10, 0);
+    doReturn(Collections.singletonList(expected))
+        .when(productBacklogItemRepository).find(10, 0);
 
-        assertThat(productBacklogService.getBacklog(10, null)).contains(expected, atIndex(0));
-    }
+    assertThat(productBacklogService.getBacklog(10, null))
+        .contains(expected, atIndex(0));
+  }
 
 }
